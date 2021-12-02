580020cbe61c5ce39de270f608f683e7738b4e80
skywalker <dzmbbs@qq.com>
2021-12-02 14:13:47 +0800

    seafile logrotate

--- docker/scripts/enterpoint.sh
+++ docker/scripts/enterpoint.sh
@@ -54,10 +54,11 @@ fi
 
 # logrotate
 if [[ -f /var/spool/cron/crontabs/root ]]; then
-    /usr/bin/crontab -l >> /scripts/logrotate-conf/logrotate-cron
+    cat /scripts/logrotate-conf/logrotate-cron >> /var/spool/cron/crontabs/root
+else
+    chmod 0644 /scripts/logrotate-conf/logrotate-cron
+    /usr/bin/crontab /scripts/logrotate-conf/logrotate-cron
 fi
-chmod 0644 /scripts/logrotate-conf/logrotate-cron
-/usr/bin/crontab /scripts/logrotate-conf/logrotate-cron
 
 
 # autorun
