55a279f169f0d03b90ef9f73fc8c7224cb000757
skywalker <dzmbbs@qq.com>
2025-03-31 18:02:16 +0800

    update renew cert

--- docker/scripts/renew_cert.sh
+++ docker/scripts/renew_cert.sh
@@ -2,8 +2,8 @@
 source /opt/dockerenv
 domain=$SEATABLE_THUMBNAIL_SERVER_HOSTNAME
 
-mkdir -p /var/www/challenges/
+/usr/bin/mkdir -p /var/www/challenges/
 
-python3 /scripts/acme-tiny-master/acme_tiny.py --account-key /opt/ssl/${domain}.account.key --csr /opt/ssl/${domain}.csr --acme-dir /var/www/challenges/ > /opt/ssl/${domain}.crt.tmp || exit
-mv /opt/ssl/${domain}.crt.tmp /opt/ssl/${domain}.crt
-nginx -s reload
+/usr/bin/python3 /scripts/acme-tiny-master/acme_tiny.py --account-key /opt/ssl/${domain}.account.key --csr /opt/ssl/${domain}.csr --acme-dir /var/www/challenges/ > /opt/ssl/${domain}.crt.tmp || exit
+/usr/bin/mv /opt/ssl/${domain}.crt.tmp /opt/ssl/${domain}.crt
+/usr/sbin/nginx -s reload
